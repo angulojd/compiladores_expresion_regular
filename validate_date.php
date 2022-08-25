d2f43d124976b8377e7e89dc9be44c3252ba7a64
Jesus Angulo <angulojd@uninorte.edu.co>
2022-08-24 22:18:51 -0500

    vainoso terminado

--- validate_date.php
+++ validate_date.php
@@ -5,11 +5,17 @@
 			echo 'No has introducido ningun valor<br>';
 			return false;
 		}else{
-			if (!preg_match("~(0[1-9]|1[012])[-/.](0[1-9]|[12][0-9]|3[01])[-/.](19|20)\d\d~",$date)){
+			$fecha = strtotime($date);
+			if (!preg_match("~([0-2][0-9]|3[0-1])[-/.](0[1-9]|[12][0-9]|3[01])[-/.](19|20)\d\d~",$date)){
 				echo 'La fecha introducida no es valida<br>';
 				return false;
 			}else{
 				echo 'Fecha introducida es valida<br>';
+				$valores = explode('/', $date);
+				if(!checkdate($valores[1], $valores[0], $valores[2])){
+					echo 'Los valores no corresponden al calendario<br>';
+					return false;
+				}
 				return true;
 			}
 		}
